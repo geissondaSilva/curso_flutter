384fd780ebe2b0aedd0ad31b7c5fcfa706051118
geisson <geisonmsilva12@gmail.com>
2019-07-31 13:06:33 -0300

    mais implementacoes contador de pessoas

--- contador_pessoas/lib/main.dart
+++ contador_pessoas/lib/main.dart
@@ -3,13 +3,47 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(new MaterialApp(
     title: "Contador de pessoas",
-    home: Column(
+    home: Stack(
       children: <Widget>[
-        Text(
-          "Pessoas: 0",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        Image.asset("images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas: 0",
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text("-1", style: TextStyle(fontSize: 40.0, color: Colors.white),),
+                      onPressed: (){},
+                    )
+                ),
+                Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text("+1", style: TextStyle(fontSize: 40.0, color: Colors.white),),
+                      onPressed: (){},
+                    )
+                ),
+              ],
+            ),
+            Text(
+              "Pode entrar",
+              style: TextStyle(color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0),
+            )
+          ],
         )
       ],
-    ),
+    )
   ));
 }
