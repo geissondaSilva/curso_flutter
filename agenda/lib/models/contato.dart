aac37d6be07af16867feb77ab6e0f22c6bb313f9
geisson <geisonmsilva12@gmail.com>
2019-10-15 22:11:36 -0300

    craicao de metodos no DAO

--- agenda/lib/models/contato.dart
+++ agenda/lib/models/contato.dart
@@ -38,6 +38,56 @@ class ContatoDao{
     });
   }
 
+  Future<Contato> saveContato(Contato contato) async {
+    Database data = await db;
+    contato.id = await data.insert(contatoTable, contato.toMap());
+    return contato;
+  }
+
+  Future<Contato> getContato(int id) async {
+    Database data = await db;
+    List<Map> maps = await data.query(
+      contatoTable,
+      columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
+      where: "$idColumn = ?",
+      whereArgs: [id]
+    );
+    if(maps.length > 0){
+      return Contato.fromMap(maps.first);
+    }else{
+      return null;
+    }
+  }
+
+  Future<int> delete(int id) async {
+    Database data = await db;
+    return await data.delete(contatoTable, where: "$idColumn = ?", whereArgs: [id]);
+  }
+
+  Future<int> updateContato(Contato obj) async {
+    Database data = await db;
+    return await data.update(contatoTable, obj.toMap(),where: "$idColumn = ?", whereArgs: [obj.id]);
+  }
+
+  Future<List<Contato>> getAll() async {
+    Database data = await db;
+    List<Map> maps = await data.rawQuery("select * from $contatoTable");
+    List<Contato> lista = List();
+    for(Map p in maps){
+      lista.add(Contato.fromMap(p));
+    }
+    return lista;
+  }
+
+  Future<int> getNumber() async {
+    Database data = await db;
+    return Sqflite.firstIntValue(await data.rawQuery("select count(*) from $contatoTable"));
+  }
+
+  Future close() async {
+    Database data = await db;
+    data.close();
+  }
 }
 
 class Contato{
